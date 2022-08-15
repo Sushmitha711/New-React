29db599f8c92c7489823fdaea7f6ce77689fa389
sushmitha711 <sushmithan012@gmail.com>
2022-08-15 17:03:24 +0530

    2 components-props

---
+++ backup-React/Comp_Prop/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react"
+class Navbar extends React.Component{
+    render(){
+        return <div>
+            <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+                    <a href="#" className="navbar-brand">Props Example</a>
+                       
+             </nav>
+             
+        </div>
+    }
+}
+export default Navbar
